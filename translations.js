c44885c8be8ca4532bebed9913795df8e9381a96
GlaGlaStarStudio <89990621+GlaGlaStarStudio@users.noreply.github.com>
2021-10-05 14:45:30 +0200

    Create translations.js

---
+++ translations.js
@@ -0,0 +1,17 @@
+const translation_en = {
+	title_about_flugubb: "About Flugubb",
+	text_about_flugubb: "Flugubb is an independant game made by the newly created studio Gla Gla Star Studio. It is available on Android.",
+	title_privacy_rules: "Privacy rules",
+	text_privacy_rules: "Fluggub respects your private life and does not collect any personnal data. Moreover, it does not request any permission on your device.",
+	download_the_game_on_google_play: "Download the game on Google play",
+	contact: "Contact"
+};
+
+const translation_fr = {
+	title_about_flugubb: "À propos de Flugubb",
+	text_about_flugubb: "Flugubb est un jeu indépendant créé par le jeune studio de création Gla Gla Star Studio. Il est disponible sur Android.",
+	title_privacy_rules: "Règles de confidentialité",
+	text_privacy_rules: "Fluggub respecte votre vie privée et ne collecte aucune donnée personnelle. De plus, il ne demande aucune autorisation à votre appareil.",
+	download_the_game_on_google_play: "Télécharger le jeu sur Google Play",
+	contact: "Contact"
+};

fbb3371a0fa1e0043e292c80209eda442a439d8c
GlaGlaStarStudio <89990621+GlaGlaStarStudio@users.noreply.github.com>
2021-10-25 17:03:26 +0200

    Update translations.js

--- translations.js
+++ translations.js
@@ -1,17 +1,17 @@
 const translation_en = {
-	title_about_flugubb: "About Flugubb",
-	text_about_flugubb: "Flugubb is an original and independent game made by the newly created studio Gla Gla Star Studio, available now on Android.",
+	title_about_flogubb: "About Flogubb",
+	text_about_flogubb: "Flogubb is an original and independent game made by the newly created studio Gla Gla Star Studio, available now on Android.",
 	title_privacy_rules: "Privacy rules",
-	text_privacy_rules: "Fluggub respects your private life and does not collect any personnal data. Moreover, it does not request any permission on your device.",
+	text_privacy_rules: "Floggub respects your private life and does not collect any personnal data. Moreover, it does not request any permission on your device.",
 	download_the_game_on_google_play: "Download the game on Google play",
 	contact: "Contact"
 };
 
 const translation_fr = {
-	title_about_flugubb: "À propos de Flugubb",
-	text_about_flugubb: "Flugubb est un jeu original et indépendant créé par le jeune studio de création Gla Gla Star Studio, disponible dès maintenant sur Android.",
+	title_about_flogubb: "À propos de Flogubb",
+	text_about_flogubb: "Flogubb est un jeufkugubb original et indépendant créé par le jeune studio de création Gla Gla Star Studio, disponible dès maintenant sur Android.",
 	title_privacy_rules: "Règles de confidentialité",
-	text_privacy_rules: "Fluggub respecte votre vie privée et ne collecte aucune donnée personnelle. De plus, il ne demande aucune autorisation à votre appareil.",
+	text_privacy_rules: "Floggub respecte votre vie privée et ne collecte aucune donnée personnelle. De plus, il ne demande aucune autorisation à votre appareil.",
 	download_the_game_on_google_play: "Télécharger le jeu sur Google Play",
 	contact: "Contact"
 };
